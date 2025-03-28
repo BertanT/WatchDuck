01d281e948afc38a8c64234f39bf80de464b4b9b
M. Bertan Tarakçıoğlu <83073748+BertanT@users.noreply.github.com>
2025-03-27 21:24:44 -0400

    Finalize the basic CI Action for releases

--- .github/workflows/scripts/build-release.sh
+++ .github/workflows/scripts/build-release.sh
@@ -30,8 +30,6 @@ set -e
 swift sdk install https://download.swift.org/swift-6.0.3-release/static-sdk/swift-6.0.3-RELEASE/swift-6.0.3-RELEASE_static-linux-0.0.1.artifactbundle.tar.gz --checksum 67f765e0030e661a7450f7e4877cfe008db4f57f177d5a08a6e26fd661cdd0bd
 swift build --configuration release --swift-sdk aarch64-swift-linux-musl
 
-exit 1
-
 # Build for macOS arm64
 swift build --configuration release --arch arm64
 
@@ -42,4 +40,15 @@ swift build --configuration release --arch x86_64
 mkdir -p .build/macos-universal/release
 lipo -create .build/arm64-apple-macosx/release/$EXEC_NAME .build/x86_64-apple-macosx/release/$EXEC_NAME\
     -output .build/macos-universal/release/$EXEC_NAME
+
+# Copy the bundle into the universal build folder
 cp -r .build/arm64-apple-macosx/release/*.bundle .build/macos-universal/release
+
+# Install the static Swift SDK for Linux Cross-Compilation
+swift sdk install https://download.swift.org/swift-6.0.3-release/static-sdk/swift-6.0.3-RELEASE/swift-6.0.3-RELEASE_static-linux-0.0.1.artifactbundle.tar.gz --checksum 67f765e0030e661a7450f7e4877cfe008db4f57f177d5a08a6e26fd661cdd0bd
+
+# Build for Linux ARM64
+swift build --configuration release --swift-sdk aarch64-swift-linux-musl
+
+# Build for Linux x86_64
+swift build --configuration release --swift-sdk x86_64-swift-linux-musl
